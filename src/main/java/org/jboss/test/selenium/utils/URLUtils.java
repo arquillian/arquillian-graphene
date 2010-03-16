4e7d87a29616e139a44aff681aec83c4ff6045c7
Lukas Fryc <lfryc@redhat.com>
2010-03-16 12:16:49 +0000

    added jboss license header (RFPL-459)

--- src/main/java/org/jboss/test/selenium/utils/URLUtils.java
+++ src/main/java/org/jboss/test/selenium/utils/URLUtils.java
@@ -1,6 +1,6 @@
-/*******************************************************************************
+/*
  * JBoss, Home of Professional Open Source
- * Copyright 2009, Red Hat, Inc. and individual contributors
+ * Copyright 2010, Red Hat, Inc. and individual contributors
  * by the @authors tag. See the copyright.txt in the distribution for a
  * full listing of individual contributors.
  *
@@ -18,7 +18,7 @@
  * License along with this software; if not, write to the Free
  * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
  * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
- *******************************************************************************/
+ */
 
 package org.jboss.test.selenium.utils;
 
