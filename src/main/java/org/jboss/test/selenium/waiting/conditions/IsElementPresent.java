d49dd23817ce7e9b944e668bca6d90137291661d
Lukas Fryc <lfryc@redhat.com>
2010-03-19 08:26:58 +0000

    implemented contextual condition IsElementPresent (impl. similar to TextEquals)

---
+++ src/main/java/org/jboss/test/selenium/waiting/conditions/IsElementPresent.java
@@ -0,0 +1,61 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2010, Red Hat, Inc. and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.jboss.test.selenium.waiting.conditions;
+
+import org.apache.commons.lang.Validate;
+import org.jboss.test.selenium.framework.AjaxSelenium;
+import org.jboss.test.selenium.framework.internal.Contextual;
+import org.jboss.test.selenium.locator.ElementLocator;
+import org.jboss.test.selenium.waiting.Condition;
+
+public class IsElementPresent implements Condition, Contextual {
+	AjaxSelenium selenium = AjaxSelenium.getCurrentContext(this);
+	ElementLocator elementLocator;
+
+	public boolean isTrue() {
+		Validate.notNull(elementLocator);
+		
+		return selenium.isElementPresent(elementLocator);
+	}
+	
+	protected IsElementPresent() {
+	}
+	
+	public static IsElementPresent getInstance() {
+        return new IsElementPresent();
+    }
+
+	public IsElementPresent locator(ElementLocator elementLocator) {
+		Validate.notNull(elementLocator);
+
+		IsElementPresent copy = copy();
+		copy.elementLocator = elementLocator;
+
+		return copy;
+	}
+
+	private IsElementPresent copy() {
+		IsElementPresent copy = new IsElementPresent();
+		copy.elementLocator = elementLocator;
+		return copy;
+	}
+}
