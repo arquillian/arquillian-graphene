9a18c766ac630bab5e5c40d163450d7b63c08b59
Lukas Fryc <lfryc@redhat.com>
2010-06-30 22:22:38 +0000

    removed old release scripts

--- perform-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CONF="-P!selenium-session,!selenium-functional-test $*"
-mvn clean release:prepare ${CONF}
-mvn release:perform
--- try-release.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-CONF="-P!selenium-session,!selenium-functional-test $*"
-mvn clean release:prepare -DdryRun=true ${CONF}
-mvn release:clean
