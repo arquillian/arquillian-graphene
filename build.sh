3657d6791daf43b0682feab968d1bd93b36d33f8
Lukas Fryc <lfryc@redhat.com>
2011-02-07 23:21:51 +0100

    removed helper scripts for build, release and verification (not needed anymore)

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn clean install -P'!selenium-session,!selenium-functional-test' $*
--- verify.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mvn -f library/functional-test/test/pom.xml clean verify -DcontainerId=tomcat6x -Dbrowser="*firefox" $*
