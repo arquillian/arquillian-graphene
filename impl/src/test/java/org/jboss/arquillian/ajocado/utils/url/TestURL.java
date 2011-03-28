4abba52b8d778c3621fe81bf4e530c8cde20f624
Lukas Fryc <lfryc@redhat.com>
2011-03-28 11:59:41 +0200

    fixed TestURL test to include asserts

--- impl/src/test/java/org/jboss/arquillian/ajocado/utils/url/TestURL.java
+++ impl/src/test/java/org/jboss/arquillian/ajocado/utils/url/TestURL.java
@@ -1,22 +1,24 @@
 package org.jboss.arquillian.ajocado.utils.url;
 
+import static org.testng.Assert.assertEquals;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.testng.annotations.Test;
 
 public class TestURL {
-    
+
     @Test
     public void testURL() throws MalformedURLException {
         String contextRoot = "http://localhost:8080";
         String contextPath = "foo/";
-        
+
         URL rootURL = new URL(contextRoot);
         URL pathURL = new URL(rootURL, contextPath);
-        
-        System.out.println(rootURL);
-        System.out.println(pathURL);
+
+        assertEquals(rootURL.toString(), "http://localhost:8080");
+        assertEquals(pathURL.toString(), "http://localhost:8080/foo/");
     }
 
 }
