0f8459869b65f298297d367d0f16bd4fb11a90b6
Lukas Fryc <lfryc@redhat.com>
2012-08-29 19:52:16 +0200

    improved javadoc for @Root and @Page

--- graphene-webdriver/graphene-webdriver-spi/src/main/java/org/jboss/arquillian/graphene/spi/annotations/Page.java
+++ graphene-webdriver/graphene-webdriver-spi/src/main/java/org/jboss/arquillian/graphene/spi/annotations/Page.java
@@ -5,6 +5,42 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation marks the given object to be instantiated and injected as Page object
+ * 
+ * <pre>
+ * {@code
+ * public class LoginForm {
+ *     
+ *     &#064;FindBy(css = ".login")
+ *     WebElement login;
+ *     
+ *     &#064;FindBy(css = ".password")
+ *     WebElement password;
+ *     
+ *     &#064;FindBy(css = ".confirmation")
+ *     WebElement confirmation;
+ *     
+ *     public void login(String login, String password) {
+ *         ...
+ *     }
+ * }
+ * 
+ * &#064;RunWith(Arquillian.class)
+ * public class Test {
+ *     
+ *     &#064;Page
+ *     LoginForm loginForm;
+ *     
+ *     &#064;Test
+ *     public void test() {
+ *         loginForm.login("login", "password");
+ *     }
+ * }
+ * </pre>
+ * 
+ * @author Juraj Huska
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface Page {
--- graphene-webdriver/graphene-webdriver-spi/src/main/java/org/jboss/arquillian/graphene/spi/annotations/Root.java
+++ graphene-webdriver/graphene-webdriver-spi/src/main/java/org/jboss/arquillian/graphene/spi/annotations/Root.java
@@ -5,8 +5,44 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation marks the <tt>WebElement</tt> field where should be injected the root of the given page,
+ * given by <tt>&#064;FindBy</tt> annotation on a injection point. 
+ * 
+ * <pre>
+ * {@code
+ * public class AutocompletionInput {
+ *     
+ *     &#064;Root
+ *     WebElement input;
+ *     
+ *     public void typeText(String text) {
+ *         input.typeText(text);
+ *     }
+ *     
+ *     
+ *     public void select(String suggestion) {
+ *         ...
+ *     }
+ * }
+ * 
+ * &#064;RunWith(Arquillian.class)
+ * public class Test {
+ *     
+ *     &#064;FindBy(css = ".autocompletion")
+ *     AutocompletionInput autocomplete;
+ *     
+ *     &#064;Test
+ *     public void test() {
+ *         autocomplete.typeText("Arq");
+ *         autocomplete.select("Arquillian");
+ *     }
+ * }
+ * </pre>
+ * 
+ * @author Juraj Huska
+ */
 @Target(ElementType.FIELD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Root {
-
 }
