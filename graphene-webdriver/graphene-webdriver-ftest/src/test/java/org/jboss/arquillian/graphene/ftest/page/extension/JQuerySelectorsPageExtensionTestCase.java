985b45d9b6f0fea2aa5bb672210b7bf953fc5b5b
Juraj Huska <jhuska@redhat.com>
2012-12-18 11:35:42 +0100

    ARQGRA-240: custom @FindBy moved from SPI to IMPL
    It will be in IMPL till API module will be substracted. I also deleted
    including of JQuery.js from staleelements.html as it threw exceptions with
    HTMLUnit driver and it is not neccessary to be included.

--- graphene-webdriver/graphene-webdriver-ftest/src/test/java/org/jboss/arquillian/graphene/ftest/page/extension/JQuerySelectorsPageExtensionTestCase.java
+++ graphene-webdriver/graphene-webdriver-ftest/src/test/java/org/jboss/arquillian/graphene/ftest/page/extension/JQuerySelectorsPageExtensionTestCase.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 import org.jboss.arquillian.drone.api.annotation.Drone;
 import org.jboss.arquillian.graphene.enricher.findby.ByJQuery;
-import org.jboss.arquillian.graphene.spi.annotations.FindBy;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
