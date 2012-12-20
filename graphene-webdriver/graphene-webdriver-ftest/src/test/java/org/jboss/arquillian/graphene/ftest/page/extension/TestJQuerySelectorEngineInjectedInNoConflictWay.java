d5caafd93bbe1402e5d04adb0413c127db656e45
Juraj Huska <jhuska@redhat.com>
2012-12-20 23:17:05 +0100

    ARQGRA-234: JQuery for Sizzle locators installed in no conflict way
    The object jQuery is installed with jquery.noConflict(true) to the
    window.Graphene.jQuery object. That is why the $ is not predefined by
    this installation neither.

---
+++ graphene-webdriver/graphene-webdriver-ftest/src/test/java/org/jboss/arquillian/graphene/ftest/page/extension/TestJQuerySelectorEngineInjectedInNoConflictWay.java
@@ -0,0 +1,58 @@
+package org.jboss.arquillian.graphene.ftest.page.extension;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+
+import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.enricher.findby.FindBy;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+@RunWith(Arquillian.class)
+public class TestJQuerySelectorEngineInjectedInNoConflictWay {
+
+    private static final String EXPECTED_$_OBJECT_VALUE = "something different than JQuery object";
+    private static final String EXPECTED_JQUERY_OBJECT_VALUE = "way different";
+
+    @FindBy(jquery = "#placeholder")
+    private WebElement placeholder;
+
+    @FindBy(jquery = ":button")
+    private WebElement show$ObjectValue;
+
+    @FindBy(jquery = ":button:eq(1)")
+    private WebElement showJQueryObjectValue;
+
+    @Drone
+    private WebDriver browser;
+
+    public void loadPage() {
+        URL page = this.getClass().getClassLoader()
+            .getResource("org/jboss/arquillian/graphene/ftest/page/extension/sampleJQueryLocatorNoConflict.html");
+        browser.get(page.toString());
+    }
+
+    @Test
+    public void test$ObejctWasNotInvadedByIncludedJQuery() {
+        loadPage();
+
+        show$ObjectValue.click();
+        String placeholderText = placeholder.getText();
+        assertEquals("The installed JQuery for sizzle locators invades predefined global variable $!", EXPECTED_$_OBJECT_VALUE,
+            placeholderText);
+    }
+
+    @Test
+    public void testJQueryObjectWasNotInvadedByIncludedJQuery() {
+        loadPage();
+
+        showJQueryObjectValue.click();
+        String placeholderText = placeholder.getText();
+        assertEquals("The installed JQuery for sizzle locators invades predefined global variable jQuery!",
+            EXPECTED_JQUERY_OBJECT_VALUE, placeholderText);
+    }
+}
