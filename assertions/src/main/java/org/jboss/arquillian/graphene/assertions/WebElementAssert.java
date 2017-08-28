c788ca66fda15277a4d4a00d21c3d88819d9e269
mookt <kealanthompson@hotmail.com>
2017-08-28 15:48:51 +0100

    final WebElementAssert.java

--- assertions/src/main/java/org/jboss/arquillian/graphene/assertions/WebElementAssert.java
+++ assertions/src/main/java/org/jboss/arquillian/graphene/assertions/WebElementAssert.java
@@ -19,12 +19,12 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
 
     public WebElementAssert hasText(String expectedText) {
         if(this.negated){
-            assertThat(this.actual.getText()).as("checking strings are not equal").overridingErrorMessage("The content of the element should not be equal to " + expectedText +", but it is").isNotEqualTo(expectedText);
+            assertThat(this.actual.getText()).as("checking strings are not equal").overridingErrorMessage("The content of the element should not be equal to " + expectedText +", but it is.").isNotEqualTo(expectedText);
             this.negated = false;
             return this;
         }
         else {
-            assertThat(this.actual.getText()).as("checking equality").overridingErrorMessage("The content of the element should be equal to" + expectedText + "but it is not").isEqualTo(expectedText);
+            assertThat(this.actual.getText()).as("checking equality").overridingErrorMessage("The content of the element should be equal to" + expectedText + "but it is not.").isEqualTo(expectedText);
             return this;
         }
     }
@@ -40,10 +40,15 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
             }
     }
 
-    //work in progress!
     public WebElementAssert hasParent(){
-        assertThat(this.actual.findElement(ByJQuery.selector("")));
-        return this;
+        if (this.negated){
+            assertThat(this.actual.findElements(By.xpath(".."))).asList().size().isEqualTo(0);
+            return this;
+        }
+        else {
+            assertThat(this.actual.findElement(By.xpath("..")));
+            return this;
+        }
     }
 
     public WebElementAssert isDisplayed(){
@@ -71,7 +76,7 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
         }
     }
 
-    //support's dropboxes, supply dropbox and text of chosen option to assert it's chosen
+    //support's dropboxes, supply dropbox and it will assert that the webelement is the chosen option
     public WebElementAssert isChosenD(Select dropdown){
         if(this.negated){
             assertThat(dropdown.getFirstSelectedOption()).as("checking supplied choice is not chosen").overridingErrorMessage("supplied choice is chosen").isNotEqualTo(this.actual);
@@ -88,12 +93,12 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
     public WebElementAssert containsText(String expectedText){
         String text = this.actual.getAttribute("value");
         if(this.negated){
-            assertThat(text).as("checking text value is not the same as the supplied string").overridingErrorMessage("The element contains " + expectedText +", the supplied string").isNotEqualTo(expectedText);
+            assertThat(text).as("checking text value is not the same as the supplied string").overridingErrorMessage("The element contains " + expectedText +", the supplied string.").isNotEqualTo(expectedText);
             this.negated = false;
             return this;
         }
         else {
-            assertThat(text).as("checking text value is the same as the supplied string").overridingErrorMessage("The element does not contain " + expectedText + ", it contains some other string").isEqualTo(expectedText);
+            assertThat(text).as("checking text value is the same as the supplied string").overridingErrorMessage("The element does not contain " + expectedText + ", it contains some other string.").isEqualTo(expectedText);
             return this;
         }
     }
@@ -113,24 +118,24 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
 
     public WebElementAssert isTypeOf(String expectedType){
         if(this.negated){
-            assertThat(this.actual.getAttribute("type")).as("checking not supplied type").overridingErrorMessage("The element contains" + expectedType + ", the supplied type").isNotEqualTo(expectedType);
+            assertThat(this.actual.getAttribute("type")).as("checking not supplied type").overridingErrorMessage("The element contains" + expectedType + ", the supplied type.").isNotEqualTo(expectedType);
             this.negated = false;
             return this;
         }
         else {
-            assertThat(this.actual.getAttribute("type")).as("checking is supplied type").overridingErrorMessage("The element does not contain" + expectedType + ", it is some other type").isEqualTo(expectedType);
+            assertThat(this.actual.getAttribute("type")).as("checking is supplied type").overridingErrorMessage("The element does not contain" + expectedType + ", it is some other type.").isEqualTo(expectedType);
             return this;
         }
     }
 
     public WebElementAssert hasCssClass(String expectedClass){
         if(this.negated){
-            assertThat(this.actual.getAttribute("class")).as("checking css class does not match supplied css class").overridingErrorMessage("The element has " + expectedClass + ", the supplied css class").isNotEqualTo(expectedClass);
+            assertThat(this.actual.getAttribute("class")).as("checking css class does not match supplied css class").overridingErrorMessage("The element has " + expectedClass + ", the supplied css class.").isNotEqualTo(expectedClass);
             this.negated = false;
             return this;
         }
         else {
-            assertThat(this.actual.getAttribute("class")).as("checking css class matches supplied class").overridingErrorMessage("The element has not the " + expectedClass + ", it contains another css class").isEqualTo(expectedClass);
+            assertThat(this.actual.getAttribute("class")).as("checking css class matches supplied class").overridingErrorMessage("The element has not the " + expectedClass + ", it contains another css class.").isEqualTo(expectedClass);
             return this;
         }
     }
@@ -161,26 +166,39 @@ public class WebElementAssert extends AbstractAssert<WebElementAssert, WebElemen
 
     public WebElementAssert textMatchesRegex(String regex){
         if(this.negated){
-            assertThat(Boolean.valueOf(this.actual.getText().matches(regex))).as("checking regex does not match text").overridingErrorMessage("The regex " + regex + " matches the text").isFalse();
+            assertThat(Boolean.valueOf(this.actual.getText().matches(regex))).as("checking regex does not match text").overridingErrorMessage("The regex " + regex + " matches the text.").isFalse();
             this.negated = false;
             return this;
         }
         else {
-            assertThat(Boolean.valueOf(this.actual.getText().matches(regex))).as("checking regex matches text").overridingErrorMessage("The regex " + regex + " does not match text").isTrue();
+            assertThat(Boolean.valueOf(this.actual.getText().matches(regex))).as("checking regex matches text").overridingErrorMessage("The regex " + regex + " does not match text.").isTrue();
             return this;
         }
     }
 
-    /*public WebElementAssert caseInsensitiveMatching(String match){
-        assertThat(this.actual.getText().contains(match));
-        return this;
-    }*/
+    public WebElementAssert caseInsensitiveMatching(String match) {
+        if (this.negated) {
+            assertThat(this.actual.getText()).as("checking string does not matches text, regardless of capitalisaton").overridingErrorMessage("The string, " + match + " is the same as the text.").isNotEqualTo(match.toLowerCase());
+            this.negated = false;
+            return this;
+        }
+        else {
+            assertThat(this.actual.getText()).as("checking string matches text, regardless of capitalisaton").overridingErrorMessage("The string, " + match + " differs to the text.").isEqualTo(match.toLowerCase());
+            return this;
+        }
+    }
 
-    /*public WebElementAssert subStringMatching(String match){
-        CharSequence sequence = (CharSequence) match;
-        assertThat(this.actual.getText().contains(sequence));
-        return this;
-    }*/
+    public WebElementAssert subStringMatching(String match){
+        if(this.negated){
+            assertThat(Boolean.valueOf(this.actual.getText().contains(match))).as("checking if substring is not in text").overridingErrorMessage("The substring " + match + " is in the text.").isFalse();
+            this.negated = false;
+            return this;
+        }
+        else {
+            assertThat(Boolean.valueOf(this.actual.getText().contains(match))).as("checking if substring is in text").overridingErrorMessage("The substring " + match + " is not in the text.").isTrue();
+            return this;
+        }
+    }
 
     public WebElementAssert isNot(){
         this.negated = true;
